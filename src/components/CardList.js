796e07afc3b0c225d872724b627a080078f2c765
a-p-h <98902547+a-p-h@users.noreply.github.com>
2022-11-05 06:02:32 -0400

    Add initial files

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <React.StrictMode>
+      <div>
+        {robots.map((user, i) => {
+          return (
+            <Card
+              key={user.id}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })}
+      </div>
+    </React.StrictMode>
+  );
+};
+
+export default CardList;
